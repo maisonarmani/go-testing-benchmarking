47a1933447859b5abe50066bc57ae41b884a9144
Maison Armani <masonarmani38@gmail.com>
2017-08-22 21:23:35 +0100

    update'

--- src/01-bug/bug_test.go
+++ src/01-bug/bug_test.go
@@ -1,10 +1,54 @@
 package main
-import "testing"
 
-func TestBug(t *testing.T){
-	if testing.Short(){
-		Bug()
-		t.Skip("Skipping Bug...")
+import (
+	"fmt"
+	"github.com/labstack/gommon/log"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRemote(t *testing.T){
+	t.Error("Error occured")
+}
+
+func TestRecorder(t *testing.T) {
+	// Manipulating fake request and respnse
+	handler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		http.Error(rw, "something failed", http.StatusInternalServerError)
+	})
+
+	req, err := http.NewRequest("GET", "http://localhost:8080", nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	fmt.Printf("%d & %s", w.Code, w.Result().Body)
+
+}
+func TestBug(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(rw, "Hello World !!!")
+	}))
+	defer server.Close()
+
+	res, err := http.Get(server.URL)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+	greeting, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Panic(err.Error())
 	}
 
+	fmt.Println("%s", greeting)
+
+}
+
+func TestTemplate(t *testing.T) {
+	templater()
 }
--- src/01-bug/main.go
+++ src/01-bug/main.go
@@ -1,9 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"github.com/labstack/gommon/log"
+	"io"
+	"text/template"
+)
+
 func main() {
-	print("Hello")
+	learningTemplating()
 }
 
-func Bug() {
-	println("I am a bug")
+const status = 10
+
+func templater() {
+
+	b := Bug(400)
+	b.startServer()
+}
+
+type Bug int
+
+func (b *Bug) startServer() {
+	i := int(*b)
+	fmt.Println(i)
+}
+
+// do some http stuff
+
+func learningTemplating() {
+
+	type Choord map[string]int
+	type Email []*map[string]string
+	type Human struct {
+		Name string
+		Age  uint8
+	}
+
+	render("file.html")
+
+}
+
+func render(view string, data ...interface{}) (func(writer io.Writer), error) {
+	const base_dir = "templates/"
+	tmp, err := template.ParseGlob(base_dir + view)
+	if err == nil {
+		tmp.ParseFiles()
+		return func(w io.Writer) {
+			tmp.Execute(w, data)
+		}, nil
+	} else {
+		log.Debug(err.Error())
+		return nil, err
+	}
 }
